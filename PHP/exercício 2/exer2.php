ebe939140b028676451c4f151b60b3113f9b2830
AndreiaRizzo <161900122+AndreiaRizzo@users.noreply.github.com>
2024-03-23 11:08:53 -0300

    exer2

---
+++ PHP/exercício 2/exer2.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 2</title>
+</head>
+<body>
+<form action="exer2resposta.php" method="post">
+        <label>Informe um valor</label>
+        <input type="number" name="valor1">
+        <label>Informe um valor</label>
+        <input type="number" name="valor2">
+        <label>Informe um valor</label>
+        <input type="number" name="valor3">
+        <label>Informe um valor</label>
+        <input type="number" name="valor4">
+        <label>Informe um valor</label>
+        <input type="number" name="valor5">
+        <label>Informe um valor</label>
+        <input type="number" name="valor6">
+        <label>Informe um valor</label>
+        <input type="number" name="valor7">
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
---
+++ PHP/exercício 2/exer2resposta.php
@@ -0,0 +1,43 @@
+<?php
+
+$menor = $_POST['valor1']; //variável criada para receber valor do arq exer1
+$valor2 = $_POST['valor2'];
+$valor3 = $_POST['valor3'];
+$valor4 = $_POST['valor4'];
+$valor5 = $_POST['valor5'];
+$valor6 = $_POST['valor6'];
+$valor7 = $_POST['valor7'];
+$pos = "1º";
+
+if ($menor > $valor2){
+    $menor = $valor2;
+    $pos = "2º";
+}
+
+
+if ($menor > $valor3){
+    $menor = $valor3;
+    $pos = "3º";
+}
+
+if ($menor > $valor4){
+    $menor = $valor4;
+    $pos = "4º";
+}
+
+if ($menor > $valor5){
+    $menor = $valor5;
+    $pos = "5º";
+}
+
+if ($menor > $valor6){
+    $menor = $valor6;
+    $pos = "6º";
+}
+
+if ($menor > $valor7){
+    $menor = $valor7;
+    $pos = "7º";
+}
+    echo "<p> $menor é o menor valor da relação e ocupa a $pos </p>";
+   
