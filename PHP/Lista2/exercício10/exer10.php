ab76836344c3ca316eb356c15faead074aa6cf46
AndreiaRizzo <161900122+AndreiaRizzo@users.noreply.github.com>
2024-04-18 19:04:38 -0300

    exer 10

---
+++ PHP/Lista2/exercício10/exer10.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 10</title>
+</head>
+<body>
+    <form action="exer10resposta.php" method="post">
+        <label>Digitar peso (Kg): </label>
+        <input type="number" name="peso"><br><br>
+
+        <label>Digitar altura (m): </label>
+        <input type="number" name="altura" step="0.01"><br><br>
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
