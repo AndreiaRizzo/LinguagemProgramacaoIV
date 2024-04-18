5de1b486188b51c72c917390c8417a868840f35a
AndreiaRizzo <161900122+AndreiaRizzo@users.noreply.github.com>
2024-04-18 17:08:59 -0300

    exer 7

---
+++ PHP/Lista2/exercício7/exer7.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 7</title>
+</head>
+<body>
+    <form action="exer7resposta.php" method="post">
+        <label>Digite o número,em metros, que você quer transformar em centímetros</label>
+        <input type="number" name="metro">
+        <button type="submit">Calcular</button>
+    </form>
+    
+</body>
+</html>
