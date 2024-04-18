52c809de2a066d8cb089695a897bd1fc5da16252
AndreiaRizzo <161900122+AndreiaRizzo@users.noreply.github.com>
2024-04-18 14:22:18 -0300

    exercicio 4

---
+++ PHP/Lista 2/exercício 4/exer4.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercício 4</title>
+</head>
+<body>
+    <form action = "exer4resposta.php" method="post">
+        <label> Digite um número para calcular a tabuada</label>
+        <input type = "number" name = "numero">
+        <button type = "submit"> Enviar</button>
+    </form>
+    
+</body>
+</html>
