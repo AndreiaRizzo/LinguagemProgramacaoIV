cce0741eb6a8061776d907327e590525fb6939a2
AndreiaRizzo <161900122+AndreiaRizzo@users.noreply.github.com>
2024-05-24 21:53:17 -0300

    banco1

--- PHP/BancodeDados/Projetobd/Produtos/index.php
+++ PHP/BancodeDados/Projetobd/Produtos/index.php
@@ -25,9 +25,13 @@ require_once("../cabecalho.php");
                 <td><?= $l['descricao'] ?></td>
                 <td><?= $l['valor'] ?></td>
                 <td><?= $l['categoria'] ?></td>
-                <td>
-                    <a href="alterar_produto.php" class="btn btn-warning">Alterar</a>
-                    <a href="excluir_produto.php" class="btn btn-danger">Excluir</a>
+                <<td>
+                    <a href="alterar_produto.php?id=<?= $l['id'] ?>" class="btn btn-warning">
+                        Alterar
+                    </a>
+                    <a href="excluir_produto.php?id=<?= $l['id'] ?>" class="btn btn-danger">
+                        Excluir
+                    </a>
                 </td>
             </tr>
             <?php
